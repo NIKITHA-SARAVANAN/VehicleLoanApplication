75b0b22252bca0b91778f88ef13598c08d7f1c37
NIKITHA-SARAVANAN <nikithasaravanan1246@gmail.com>
2026-03-26 00:29:14 +0530

    demographic information and KYC details captured

--- force-app/main/default/lwc/vehicleLoanApplication/vehicleLoanApplication.js
+++ force-app/main/default/lwc/vehicleLoanApplication/vehicleLoanApplication.js
@@ -1,6 +1,6 @@
 import { LightningElement,wire } from 'lwc';
 import getVehicles from '@salesforce/apex/VehicleLoanController.getVehicles';
-import saveRecord from '@salesforce/apex/VehicleLoanController.saveRecord';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class VehicleLoanApplication extends LightningElement {
     vehicles;
     productName;
@@ -10,7 +10,28 @@ export default class VehicleLoanApplication extends LightningElement {
     loanAmount;
     tenure;
     masterId;
+
+    aadhaar;
+    pan;
+    employmentType;
+    gst;
+    salary;
+    companyAddress;
+    employmentType = '';
+    isSelfEmployed = false;
     showForm = false;
+    showPopup = false;
+    showCustomerForm = false;
+    showKYCForm = false;
+     salutationsList = [
+        { label: 'Mr.', value: 'Mr.' },
+        { label: 'Ms.', value: 'Ms.' },
+        { label: 'Mrs.', value: 'Mrs.' },
+    ];
+    employmentOptions = [
+        { label: 'Salaried', value: 'salaried' },
+        { label: 'Self Employed', value: 'self_employed' }
+    ];
     @wire(getVehicles)
     wiredVehicles({ data, error }) {
         if (data) {
@@ -26,29 +47,97 @@ export default class VehicleLoanApplication extends LightningElement {
 
         this.showForm = true;
     }
+    handleDownPayment(event) {
+    this.downPayment = event.target.value;
+}
      handleLoan(event) {
-        this.loanAmount = parseFloat(event.target.value);
+        this.loanAmount = event.target.value;
+         if (this.loanAmount > this.price) {
+        event.target.setCustomValidity('Loan amount cannot be greater than price');
+        }   
+        else {
+        event.target.setCustomValidity('');
+        }
+        event.target.reportValidity();
     }
 
     handleTenure(event) {
-        this.tenure = parseInt(event.target.value);
+        this.tenure = event.target.value;
     }
-     handleSubmit() {
-        saveRecord({
-            name: this.productName,
-            make: this.make,
-            engine: this.engine,
-            price: this.price,
-            loanAmount: this.loanAmount,
-            tenure: this.tenure,
-            masterId: this.masterId
-        })
-        .then(() => {
-            alert('Record Created Successfully');
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    }
+    handleNext() {
+        const inputs = this.template.querySelectorAll('lightning-input');
+        let isValid = true;
 
+        inputs.forEach(input => {
+            if (!input.checkValidity()) {
+                input.reportValidity(); 
+                isValid = false;
+            }
+        });
+        if (!isValid) {
+            return;
+        }
+        const event = new ShowToastEvent({
+            title: 'Congratulations 🎉',
+            message: 'You are eligible for Preapproval',
+            variant: 'success',   
+            mode: 'dismissable'   
+        });
+
+        this.dispatchEvent(event);
+        this.showForm = false;
+        this.showCustomerForm = true;
+    }
+    handleDOBChange(event) {
+        const selectedDate = new Date(event.target.value);
+        const today = new Date();
+
+        if (selectedDate > today) {
+            event.target.setCustomValidity('Date of Birth cannot be in the future');
+        } else {
+            event.target.setCustomValidity('');
+        }
+
+        event.target.reportValidity();
+    }
+    get salutationOptions() {
+        return this.salutationsList;
+    }
+    handleMobileChange(event) {
+        const value = event.target.value;
+
+        if (!/^[0-9]{10}$/.test(value)) {
+            event.target.setCustomValidity('Enter a valid 10-digit mobile number');
+        } else {
+            event.target.setCustomValidity('');
+        }
+
+        event.target.reportValidity();
+    }
+    handleAadhaarChange(event) {
+        this.aadhaar = event.target.value;
+    }
+    handlePanChange(event) {
+        this.pan = event.target.value.toUpperCase();
+        event.target.value = this.pan;
+    }
+    handleEmploymentChange(event) {
+        this.employmentType = event.detail.value;
+        this.isSelfEmployed = this.employmentType === 'self_employed';
+    }
+    handleSalaryChange(event) {
+        this.salary = event.target.value;
+    }
+    handleGstChange(event) {
+        this.gst = event.target.value;
+    }
+    handleCompanyAddressChange(event) {
+        this.companyAddress = event.target.value;
+    }
+    handleNextButton() {
+    this.showCustomerForm = false;
+    this.showKYCForm = true;
+}
+    
+    
 }
